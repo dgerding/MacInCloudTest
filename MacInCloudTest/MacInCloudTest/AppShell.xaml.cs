b65e6c6fdf10a676ce4896913268641a2b03b0d3
David Gerding <dave@gerding.us>
2020-09-22 13:25:18 -0500

    Add project files.

---
+++ MacInCloudTest/MacInCloudTest/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using MacInCloudTest.ViewModels;
+using MacInCloudTest.Views;
+using Xamarin.Forms;
+
+namespace MacInCloudTest
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ MacInCloudTest/MacInCloudTest/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using MacInCloudTest.Models;
+using MacInCloudTest.ViewModels;
+
+namespace MacInCloudTest.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
